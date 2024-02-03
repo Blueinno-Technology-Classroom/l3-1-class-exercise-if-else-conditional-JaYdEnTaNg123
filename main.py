62d2488bd65d03bcbabca2e265332ad9f5ae6d54
JaYdEnTaNg123 <tangjayden999@gmail.com>
2024-02-03 12:16:41 +0800

    `¡™£¢∞§¶•ªº–≠œ∑´®†¥¨ˆøπ“‘«åß∂ƒ˙∆˚¬…æ≈ç√∫˜µ≤≥÷`⁄€‹›ﬁﬂ‡°·‚—±Œ„´‰ˇÁ¨ˆØ∏”’»ÅÍÎÏ˝ÓÔÒÚÆ¸˛Ç◊ı˜Â¯˘¿

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 ##################################################
 '''
 Q1:
-a: 
-b:  
-c:  
-d: 
-e: 
-f: 
-g: 
-h: 
-i: 
+a: true
+b:  false
+c:  true
+d: false
+e: true
+f: false
+g: true
+h: true
+i: 	true
 '''
 
 ##################################################
 '''
 Q2:
-a:  
-b:  
-c:  
-d: 
-e: 
-f: 
-g: 
-h: 
-i: 
-j: 
-k: 
+a:  y > 0
+b:  z != 0
+c:  y > z
+d:  not(z>0)
+e:  x-y<z-y
+f:  z %2==1
+g:  x %2==0
+h:  y %2==z
+i:  not(z>0)and (z<10)
+j:  (x>0) and (z<0)
+k:  (x %2==0) and (y %2==1)
 '''
 
 ##################################################
@@ -38,7 +38,7 @@ number = int(input('Give me a number: '))
 if number % 2 == 0:
     if number % 3 == 0:
         print("Divisible by 6.")
-    else:
-        print("Odd number.")
+else:
+        print("even number.")
 
 ##################################################
